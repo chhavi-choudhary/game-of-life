90e11de39118da78c3d42645aa84855700a9d619
Chhavi <Chhavi.choudhary75@gmail.com>
2021-09-30 22:39:58 +0200

    grid template

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { useState } from 'react';
 import Grid from './components/Grid';
 import './style.css';
 
+
+
+const numRows=30;
+const numColumns=30;
+const rows=[];
+const initialGrid=(numRows, numColumns)=>{
+   for(let i=0;i<numRows;i++){
+        rows.push(Array.from(Array(numColumns),()=>0))
+    }
+     return rows;
+}
+initialGrid(numRows, numColumns);
+
+
+
 function App() {
   const[generation, setGeneration]=useState(0)
   return (
     <div className="App">
      <h1>Conway's Game Of Life !!</h1>
-    <Grid/>
+    <Grid gridStr={initialGrid()}/>
     <h2>Generations:{generation}</h2>
     </div>
+    
   );
 }
 
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../style.css';
 
-const numRows=30;
-const numColumns=30;
 
-function Grid() {
-    const rows=[];
-    for(let i=0;i<numRows;i++){
-        rows.push(Array.from(Array(numColumns),()=>0))
-     }
-     console.log(rows)
+function Grid({gridStr}) {
+const [grid, setGrid]=useState(gridStr)
+
+
     return (
-        <>
-            <p>Create a Grid</p>
-            <div>
-                
+        <div >
+            <h2>Create a Grid</h2>
+            <div style={{display:"grid",
+        gridTemplateColumns:`repeat(30, 20px)`}} className="disp">  
+                {grid.map((rows, i)=>
+                rows.map((columns, j)=><div className="gridstr" key={`${i}-${j}`}></div>))}  
             </div>
-        </>
+        </div>
     )
 }
 export default Grid;
